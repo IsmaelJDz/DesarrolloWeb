2feb3f8de99a0fbcab79419c43da31771a804e90
IsmaelJdz <ismael_br7@hotmail.com>
2019-02-01 13:32:31 -0600

    Maquetacion de targetas de producto

---
+++ js/main.js
@@ -0,0 +1,9 @@
+(function(){
+    var tId = setInterval(function() {
+        if (document.readyState == "complete") onComplete()
+    }, 11);
+    function onComplete(){
+        clearInterval(tId);
+        alert("loaded!");
+    };
+})()
